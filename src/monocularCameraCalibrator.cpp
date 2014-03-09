41b8e94da47df5cf23dc6dc4a4364f0bebb249ef
paschalidoud <paschalidoud@gmail.com>
2014-03-09 14:20:33 +0200

    added comments to class monocularCameraCalibrator

--- src/monocularCameraCalibrator.cpp
+++ src/monocularCameraCalibrator.cpp
@@ -1,13 +1,21 @@
 #include "monocularCameraCalibrator.h"
 
+/**
+ @brief Constructor
+ @param camerIndicator [int] number of camera device 
+ (default camera device 0)
+ @return void
+*/
+
 monocularcameraCalibrator::monocularcameraCalibrator(int cameraIndicator)
 {
-    std::cout<<"monocularcameraCalubratir instance created"<<std::endl;
+    std::cout<<"MonocularcameraCalubrator instance created"<<std::endl;
     frameHeight=480;
     frameWidth=640;
     
-    //indicate whether the complete board was foundor not (=0)
-    FoundFrameIndicator=0;
+    //!< indicate whether the complete board was foundor not (=0)
+    foundFrameIndicator=0;
+
     CameraCapture=new cv::VideoCapture(cameraIndicator);
     if(!CameraCapture->isOpened())
     {
@@ -27,67 +35,91 @@ monocularcameraCalibrator::monocularcameraCalibrator(int cameraIndicator)
  
 }
 
-/*
- *Function that takes frames from each camera
- */  
+/**
+ @brief Function that takes frames from each camera
+ @return void
+*/
+
 void monocularcameraCalibrator::createSnapshot()
 {
    
-	    //retrieve data(shnapshot) from camera
-        CameraCapture->grab();
-        CameraCapture->retrieve(frame);
-        //turn image into gray
-        cv::cvtColor(frame, greyframe, CV_RGB2GRAY);
+  //!< Retrieve data(shnapshot) from camera
+  CameraCapture->grab();
+  CameraCapture->retrieve(frame);
+  //!<Turn image into gray
+  cv::cvtColor(frame, greyframe, CV_RGB2GRAY);
 }
-/*
- * Function that refreshes frames
- */ 
- void monocularcameraCalibrator::refreshWindow()
- {
-        cv::imshow("snapshot",frame);
-       
- }
+
+/**
+ @brief Function that refreshes frames
+ @return void
+*/
+
+void monocularcameraCalibrator::refreshWindow()
+{
+      cv::imshow("snapshot",frame);
+}
+
+/**
+ @brief Function that detects and draws corners either in chessboard 
+ or in circleboard
+ @param patternchoice [int], type of pattern we want to use, 1 for 
+ chessboard and 2 for circleboard
+ @param patternsize [cv::Size] size of chessboard
+ @param circlepatternsize [cv::Size] size of circleboard
+ @return void
+*/
 
 void monocularcameraCalibrator::detectChessboard(int patternchoice,cv::Size patternsize,cv::Size circlepatternsize)
 {
   int patternflag=2;
+  
   if(patternchoice==1)
   {
-
-    //find and draw corners for the camera's snapshot
-      patternflag=cv::findChessboardCorners(greyframe,patternsize,corners,
-           cv::CALIB_CB_ADAPTIVE_THRESH+cv::CALIB_CB_NORMALIZE_IMAGE/*+CALIB_CB_FAST_CHECK*/);
-  // improve the found corners' coordinate accuracy for chessboard
-  if(patternflag==0 && (corners.size()!=0))
-  {
-      cv::cornerSubPix(greyframe,corners, cv::Size(11, 11), cv::Size(-1, -1),
-                   cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
-  }
-    //draw the corners
-       cv::drawChessboardCorners(frame,patternsize, cv::Mat(corners),patternflag);
+    //!<Find and draw corners for the camera's snapshot
+    patternflag=cv::findChessboardCorners(greyframe,patternsize,corners,
+         cv::CALIB_CB_ADAPTIVE_THRESH+cv::CALIB_CB_NORMALIZE_IMAGE/*+CALIB_CB_FAST_CHECK*/);
+         
+    //!< Improve the found corners' coordinate accuracy for chessboard
+    if(patternflag==0 && (corners.size()!=0))
+    {
+        cv::cornerSubPix(greyframe,corners, cv::Size(11, 11), cv::Size(-1, -1),
+                     cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+    }
+    //!< Draw the corners
+    cv::drawChessboardCorners(frame,patternsize, cv::Mat(corners),patternflag);
   }
   else if(patternchoice==2)
   {
-      //find and draw corners for the left camera's snapshot
-      patternflag= cv::findCirclesGrid(greyframe,circlepatternsize,centers,
-                cv::CALIB_CB_ASYMMETRIC_GRID/*+CALIB_CB_CLUSTERING*/);
-    //draw the corners
-       cv::drawChessboardCorners(frame,circlepatternsize, cv::Mat(centers),patternflag);
+    //!<Find and draw corners for the camera's snapshot
+    patternflag= cv::findCirclesGrid(greyframe,circlepatternsize,centers,
+              cv::CALIB_CB_ASYMMETRIC_GRID/*+CALIB_CB_CLUSTERING*/);
+    //!< Draw the corners
+     cv::drawChessboardCorners(frame,circlepatternsize, cv::Mat(centers),patternflag);
   }
   else
   {
       std::cout<<"There is not such a pattern choise"<<std::endl;
   }
-
 }
-/*
-*In this function we set object kai image points!object points refer to the real 3d coordinates of pattern points
-*in the calibration pattern,whereas image points are 2d cordinates and refer to the projections of calibration pattern
-*points
+
+/**
+ @brief In this function we set object kai image points. Object points 
+ refer to the real 3d coordinates of pattern points in the calibration 
+ pattern, whereas image points are 2d cordinates and refer to the 
+ projections of calibration pattern points
+ @param patternchoice [int], type of pattern we want to use, 1 for 
+ chessboard and 2 for circleboard
+ @param patternsize [cv::Size] size of chessboard
+ @param circlepatternsize [cv::Size] size of circleboard
+ @param squareSize [int] size of each squre of chessborad
+ @param circleSize [int] raduis of each circle in circleboard
+ @return void
 */
+
 void monocularcameraCalibrator::setPoints(int patternchoice,cv::Size patternsize,cv::Size circlepatternsize,int squareSize,int circleSize)
 {
-    //set object points
+    //!< Set object points
     std::vector<cv::Point3f> obj;
     if(patternchoice==1)
     {
@@ -110,7 +142,8 @@ void monocularcameraCalibrator::setPoints(int patternchoice,cv::Size patternsize
         }
     }
     objectPoints.push_back(obj);
-   //set imagePoints
+   
+   //!< Set imagePoints
    if(patternchoice==1)
     {
         imagePoints.push_back(corners);
@@ -122,22 +155,38 @@ void monocularcameraCalibrator::setPoints(int patternchoice,cv::Size patternsize
     std::cout<<"Object and Image Points are stored"<<std::endl;
 }
 
+/**
+ @brief Function that calculates camerMatrix, distortion coeefficients
+ and RMS error
+ @return void
+*/
+
 void monocularcameraCalibrator::calibratefunction()
 {
-    cv::Size imageSize=greyframe.size();
+  cv::Size imageSize=greyframe.size();
 	std::vector<cv::Mat> rvecs;
 	std::vector<cv::Mat> tvecs;
-    double cameraReprojectionError;
-    std::cout<<"Camera Calibration begins!"<<std::endl;
+  double cameraReprojectionError;
+  std::cout<<"Camera Calibration begins!"<<std::endl;
 
-    cameraReprojectionError = cv::calibrateCamera(objectPoints, imagePoints, imageSize, cameraMatrix[0],
-            distCoeffs[0], rvecs, tvecs,CV_CALIB_FIX_K4| CV_CALIB_FIX_K5);
-    std::cout<<"Calibration for left camera ended! RMS error:" <<cameraReprojectionError<<std::endl;;
-    calibrationflag=true;
-    std::cout<<"Camera is calibrated"<<std::endl;
-    this->savedata();
-    this->undistort();
+  cameraReprojectionError = cv::calibrateCamera(objectPoints, imagePoints, imageSize, cameraMatrix[0],
+          distCoeffs[0], rvecs, tvecs,CV_CALIB_FIX_K4| CV_CALIB_FIX_K5);
+  std::cout<<"Calibration for left camera ended! RMS error:" <<cameraReprojectionError<<std::endl;
+  
+  calibrationflag=true;
+  std::cout<<"Camera is calibrated"<<std::endl;
+  
+  this->savedata();
+  this->undistort();
 }
+
+/**
+ @brief Function that computes the undistortion and rectification 
+ transformation map and applies a generic geometrical transformation 
+ to an image.
+ @return void
+*/
+
 void monocularcameraCalibrator::undistort()
 {
     cv::Size imageSize=greyframe.size();
@@ -146,6 +195,12 @@ void monocularcameraCalibrator::undistort()
         cv::getOptimalNewCameraMatrix(cameraMatrix[0], distCoeffs[0], imageSize, 1, imageSize, 0),imageSize, CV_16SC2, map1, map2);
     cv::remap(greyframe, calibratedframe, map1, map2, cv::INTER_LINEAR);
 }
+
+/**
+ @brief Function that saves calibration data in a file named
+ intrinsics_eye.yml
+ @return void
+*/
 void monocularcameraCalibrator::savedata()
 {
 	cv::FileStorage fs("intrinsics_eye.yml", CV_STORAGE_WRITE);
@@ -158,6 +213,19 @@ void monocularcameraCalibrator::savedata()
 		std::cout << "Error: can not save the intrinsic parameters"<<std::endl;
 }
 
+/**
+ @brief Function that calibrates one camera. First function 
+ setpoints() is called several times (approximately 20-30 times)
+ and afterwards calibratefunction() to calculate and save all camera's
+ intrinsics parameters.
+ @param patternchoice [int], type of pattern we want to use, 1 for 
+ chessboard and 2 for circleboard
+ @param patternsize [cv::Size] size of chessboard
+ @param circlepatternsize [cv::Size] size of circleboard
+ @param squareSize [int] size of each squre of chessborad
+ @param circleSize [int] raduis of each circle in circleboard
+ @return void
+*/
 void monocularcameraCalibrator:: monocularCalibrate(int patternchoice,cv::Size patternsize,cv::Size circlepatternsize,int squareSize,int circleSize)
 {
 	int KeyPressed=255; 
@@ -171,16 +239,16 @@ void monocularcameraCalibrator:: monocularCalibrate(int patternchoice,cv::Size p
       refreshWindow();
               
       KeyPressed=cvWaitKey(10) & 255;
-      //different choices
-      if(KeyPressed==27) //KeyPressed==esc
+      //!< different choices
+      if(KeyPressed==27) //!< KeyPressed==esc
       {
          break;
       }
-      if (KeyPressed==112) //KeyPressed==p
+      if (KeyPressed==112) //!< KeyPressed==p
       {
         setPoints(patternchoice,patternsize,circlepatternsize,squareSize,circleSize);
       }
-      if(KeyPressed==115) //KeyPressed==s
+      if(KeyPressed==115) //!< KeyPressed==s
       {
          calibratefunction();
          cv::imshow("calibrated snapshot",greyframe);
@@ -188,6 +256,8 @@ void monocularcameraCalibrator:: monocularCalibrate(int patternchoice,cv::Size p
        
      }
 }
+
+//!< Destructor
 monocularcameraCalibrator::~monocularcameraCalibrator()
 {
 	std::cout<<"monocularcameraCalibrator instance deleted"<<std::endl;
